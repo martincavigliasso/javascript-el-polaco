a677b71664175c27e96f24fe8a957f308b4b077b
martincavigliasso <martincavigliasso@gmail.com>
2022-08-16 18:41:20 -0300

    funciones

---
+++ js/categorias.js
@@ -0,0 +1,44 @@
+const categorias = [
+
+    {id:1,nombre:"aceitunas"},
+    {id:2,nombre:"aceites"},
+    {id:3,nombre:"mayonesas"},
+    {id:4,nombre:"sales"},
+    {id:5,nombre:"galletitas"},
+
+]
+
+const productos = [
+
+    {id:1,nombre:"Huevos", precio:200, categoria:1, stock:100},
+    {id:2,nombre:"Pan", precio:300, categoria:1, stock:10},
+    {id:3,nombre:"Queso", precio:400, categoria:1, stock:0},
+
+    {id:4,nombre:"Coca Cola", precio:400, categoria:2, stock: 20},
+    {id:5,nombre:"Jugo", precio:600, categoria:2, stock: 30},
+    {id:6,nombre:"Agua", precio:300, categoria:2, stock: 40},
+    {id:7,nombre:"Soda", precio:200, categoria:2,  stock: 10},
+    {id:8,nombre:"Malta", precio:100, categoria:2,  stock: 0},
+
+
+    {id:9,nombre:"aceitunas", precio:1200, categoria:3, stock: 10},
+    {id:10,nombre:"sal", precio:800, categoria:3, stock: 30},
+    {id:11,nombre:"fideos", precio:1300, categoria:3, stock: 50},
+    {id:12,nombre:"cervezas", precio:500, categoria:3,  stock: 0},
+    
+    {id:13,nombre:"gaseosa", precio:500, categoria:4, stock: 10},
+    {id:14,nombre:"papas en bolsa", precio:800, categoria:4, stock: 20},
+    {id:15,nombre:"salsas", precio:400, categoria:4, stock: 50},
+    
+
+    {id:16,nombre:"Pasta de Dientes", precio:400, categoria:5, stock: 20},
+    {id:17,nombre:"Jabón Cuerpo", precio:600, categoria:5, stock: 30},
+    {id:18,nombre:"Jabón Loza", precio:300, categoria:5, stock: 0},
+    {id:19,nombre:"Detergente", precio:200, categoria:5,  stock: 10},
+   
+    
+
+
+
+
+]
--- js/funciones.js
+++ js/funciones.js
@@ -5,28 +5,83 @@ const productos = ['aceitunas' , 'salsas' , 'masitas' , 'harinas' , 'aceites']
 
 const carrito = []
 
-function listaProductos(){
-    //debugger
-    toal = parseInt (prompt("ingresa un nro."))
-    if (productos.length < total)
-    for(let i = 0; i < total; i++){
-        console.log(productos[i])
+
+
+
+
+
+function init(){
+    titulo("el polaco");
+    mostrarMenu();
+    escribirMensaje();
+      
     }
+    init()
+  
+    function titulo() {
+      let h1 = document.createElement('h1')
+      h1.innerText= 'Bienvenido a distribuidora el Polaco'
+      document.body.append(h1);
+    }
+    titulo()
+  
+  
+  
+    function cambiarClase() {
+      let botones = document.getElementsByTagName('button')
+      for (const boton of botones) {
+        boton.className = 'stylebtn'
+      }
+    }
+    cambiarClase()
+
+    function mensaje() {
+      let p = document.createElement('p')
+      p.innerText = 'Por favor, selecciona una categoría para continuar'
+      document.body.append(p)
+    } 
+    mensaje() 
+
+    function mostrarMenu()
+{
+ 
+   categorias.forEach((categoria)=>{
+     const myBtn = document.createElement("button");
+     myBtn.setAttribute("class", "styledBtn");
+     myBtn.innerHTML=categoria.nombre;
+     myBtn.addEventListener("click", ()=>mostrarProductos(categoria.id));
+     main.appendChild(myBtn);
+   })
 }
-listaProductos()
-
-
-function agregarProductos(){
-    let nuevoProductos = prompt("ingresa el nuevo producto:")
-     productos.push(nuevoProductos)
-} 
-
-agregarProductos()
-
-function quitarPoductos(){
-    let aQuitar =  parseInt(prompt ("ingrese el ID del producto a quitar"))
-    let resultado = productos.splice (aQuitar, 3)
-    console.warn ("se elimino el producto:", resultado)
+function escribirMensaje()
+{
+  const nodoMensaje = document.createElement("p");
+  nodoMensaje.innerHTML= "Por favor, selecciona una categoría para continuar:";
+  main.appendChild(nodoMensaje);
 }
-quitarPoductos() 
 
+
+function mostrarProductos(idCategoria)
+{
+    const productosFiltrados = filtrarProductos(idCategoria);
+    let cadena ='';
+    productosFiltrados.forEach((element)=>{
+      cadena+=`<div>
+  
+      Nombre Producto: ${element.nombre}<br>
+      Precio Producto: ${element.precio}<br>
+  
+      
+      </div>`
+  
+      document.querySelector("#productos").innerHTML=cadena;
+    }); 
+
+}
+
+function filtrarProductos(idCategoria)
+{
+
+    return productos.filter(producto=>producto.categoria===idCategoria);
+
+}
