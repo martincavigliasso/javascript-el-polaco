8740cb59b0f721d4fc0f93d6b4d753c02f7b129c
martincavigliasso <martincavigliasso@gmail.com>
2022-08-16 16:32:23 -0300

    correccion

--- js/funciones.js
+++ js/funciones.js
@@ -30,22 +30,3 @@ function quitarPoductos(){
 }
 quitarPoductos() 
 
-function creoID() { return parseInt(Math.random() * 10000) }
-
-const codigo = document.querySelector("#codigo")
-const descripcion = document.querySelector("#descripcion")
-const importe = document.querySelector("#importe")
-
-const btnNuevo = document.querySelector("#btnNuevo")
-const btnGuardar = document.querySelector("#btnGuardar")
-
-btnNuevo.addEventListener("click", nuevoProducto)
-
-
-function nuevoProducto() {
-    codigo.value = creoID()
-    descripcion.value = ""
-    importe.value = ""
-    descripcion.focus()
-} 
-nuevoProducto()
